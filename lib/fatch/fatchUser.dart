89979290d2a178559a2d762a8caac0a91f4ed9bc
Bintang Ramadhan <80246874+bintangr28@users.noreply.github.com>
2021-03-09 10:11:14 +0700

    Update fatchUser.dart

--- lib/fatch/fatchUser.dart
+++ lib/fatch/fatchUser.dart
@@ -5,17 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 Future<User> fetchUser() async {
-  final response =
-      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+  final response = await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     return User.fromJson(jsonDecode(response.body));
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
+    throw Exception('Gagal menghubungkan');
   }
 }
 
